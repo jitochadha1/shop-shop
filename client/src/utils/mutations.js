1c71ccb4ca2a84434f6d042eb84da0cc19d7168a
Jito Chadha <jitochadha@gmail.com>
2021-05-17 12:43:25 -0700

    f

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
+      
       token
       user {
         _id
